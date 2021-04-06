c899fb08b10b59dff6131f4ed3b9a371a2077b00
TheBottle <thebottle228@icloud.com>
2021-04-07 00:12:35 +0600

    //

--- Usage/Program.cs
+++ Usage/Program.cs
@@ -11,8 +11,7 @@ namespace Usage
         public static string RandomString(int length)
         {
             const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-            return new string(Enumerable.Repeat(chars, length)
-                .Select(s => s[random.Next(s.Length)]).ToArray());
+            return new string(Enumerable.Repeat(chars, length).Select(s => s[random.Next(s.Length)]).ToArray());
         }
 
         static void Main(string[] args)
